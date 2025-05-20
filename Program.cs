16a25063ff07f5ac8138f87ca99191f4dcf7c261
BautistaAlvarez <alejobaltazargonzalez@gmail.com>
2025-05-20 15:29:17 -0300

    Commit del ejercicio1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b=a;
+
+Console.WriteLine("Valor de a:"+a);
+Console.WriteLine("Valor de b:"+b);
+
+//ejercicio1, instalar extension devkit 
+Console.WriteLine("Ingrese un numero de 2 cifras mayor a 0:");
+int valIngresado;
+int ultimoNum;
+int numInvertido = 0;
+string? stringIngresado = Console.ReadLine();
+if(int.TryParse(stringIngresado, out valIngresado)){
+
+    while (valIngresado > 0)
+    {
+            ultimoNum = valIngresado%10;
+            numInvertido = numInvertido*10 + ultimoNum;
+            valIngresado /= 10;
+    }
+    Console.WriteLine($"Numero invertido: {numInvertido}");
+}else{
+    Console.WriteLine("Invalido");
+}
+
+//ejercicio2
