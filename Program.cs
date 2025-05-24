fc5fffe7996e69ec5acc248603b94eaee5fe982e
Juan <AlvarezBautista26@hotmail.com>
2025-05-24 19:37:28 -0300

    Commit del ejercicio2

--- Program.cs
+++ Program.cs
@@ -28,3 +28,88 @@ if(int.TryParse(stringIngresado, out valIngresado)){
 }
 
 //ejercicio2
+int  continuar = 0;
+do
+{
+    Console.WriteLine("Ingrese 1 para Sumar, 2 para Restar, 3 para Multiplicar o 4 para Dividir");
+    string? operacionStr = Console.ReadLine();
+    string? num1Str, num2Str;
+    int num1, num2, resultado, operacion;
+    if (int.TryParse(operacionStr, out operacion) && operacion > 0 && operacion < 5)
+    {
+        switch (operacion)
+        {
+
+            case 1:
+                Console.WriteLine("Ingrese 2 numeros para la operacion:");
+                num1Str = Console.ReadLine();
+                num2Str = Console.ReadLine();
+                if (int.TryParse(num1Str, out num1) && int.TryParse(num2Str, out num2))
+                {
+                    resultado = num1 + num2;
+                    Console.WriteLine("La suma de " + num1 + " y de " + num2 + " es: " + resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Numeros invalidos");
+                }
+                break;
+            case 2:
+                Console.WriteLine("Ingrese 2 numeros para la operacion:");
+                num1Str = Console.ReadLine();
+                num2Str = Console.ReadLine();
+                if (int.TryParse(num1Str, out num1) && int.TryParse(num2Str, out num2))
+                {
+                    resultado = num1 - num2;
+                    Console.WriteLine("La resta de " + num1 + " y de " + num2 + " es: " + resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Numeros invalidos");
+                }
+                break;
+            case 3:
+                Console.WriteLine("Ingrese 2 numeros para la operacion:");
+                num1Str = Console.ReadLine();
+                num2Str = Console.ReadLine();
+                if (int.TryParse(num1Str, out num1) && int.TryParse(num2Str, out num2))
+                {
+                    resultado = num1 * num2;
+                    Console.WriteLine("El resultado de " + num1 + " y de " + num2 + " es: " + resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Numero Invalido");
+                }
+                break;
+            case 4:
+                Console.WriteLine("Ingrese 2 numeros para la operacion:");
+                num1Str = Console.ReadLine();
+                num2Str = Console.ReadLine();
+                if (int.TryParse(num1Str, out num1) && int.TryParse(num2Str, out num2))
+                {
+                    if (num2 != 0)
+                    {
+                        resultado = num1 / num2;
+                        Console.WriteLine("El resultado de " + num1 + " y de " + num2 + " es: " + resultado);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El divisor no puede ser 0");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Numeros invalidos");
+                }
+                break;
+        }
+        Console.WriteLine("Desea realizar otra operacion? Ingrese 0 para terminar y otro numero para repetir");
+        string? continuarStr = Console.ReadLine();
+        int.TryParse(continuarStr, out continuar);
+    }
+    else
+    {
+        Console.WriteLine("Numero invalido");
+    }
+} while (continuar != 0);
